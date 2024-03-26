c4170635820049cc81a7b62e5e6a2b55436b1c1e
JackShi <jackshi.2023@gmail.com>
2024-03-26 08:56:16 +0800

    feat: xtransfer

---
+++ Records/面试题/xt.js
@@ -0,0 +1,63 @@
+// react hooks实现一个计数器 每秒钟+1
+// function Counter() {
+//     const [count, setCount ] = useState(0);
+
+//     // useEffect(()=>{
+//     //     const timer = setInternal(()=>{
+//     //         setCount(count+1);
+//     //     },1000);
+
+//     //     return () => clearInternal(timer);
+//     // },[])
+
+//     return (
+//         <div>{count}</div>;
+//     )
+// }
+
+// [
+//     {
+//         id: 1,
+//         text: '节点1',
+//         parentId: 0 //这里用0表示为顶级节点
+//     },
+//     {
+//         id: 2,
+//         text: '节点1_1',
+//         parentId: 1 //通过这个字段来确定子父级
+//     }
+// ]
+
+// 转成
+// [
+//     {
+//         id: 1,
+//         text: '节点1',
+//         parentId: 0,
+//         children: [
+//             {
+//                 id:2,
+//                 text: '节点1_1',
+//                 parentId:1
+//             }
+//         ]
+//     }
+// ]
+
+function jsToTree(data) {
+  let result = [];
+
+  let map = {};
+  data.forEach((item) => {
+    map[id] = item;
+  });
+
+  data.forEach((item) => {
+    let parent = map[item.parentId];
+    if (parent) {
+      (parent.children || (parent.children = [])).push(item);
+    } else {
+      result.push(item);
+    }
+  });
+}
