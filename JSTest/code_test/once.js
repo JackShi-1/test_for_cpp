761edac05a268f3886f736bfef7e516b4795c36b
shizhenyu.2021 <shizhenyu.2021@bytedance.com>
2025-01-26 09:36:00 +0800

    feat: features

--- JSTest/code_test/once.js
+++ JSTest/code_test/once.js
@@ -7,7 +7,7 @@ const fetchUser = (id) => {
     }, 5000);
   });
 };
- 
+
 const cache = {};
 const cacheFetchUser = (id) => {
   if (cache[id]) {
@@ -17,37 +17,48 @@ const cacheFetchUser = (id) => {
   return cache[id];
 };
 
-
 // 效果
-cacheFetchUser(3).then((id) => console.log(id))
-cacheFetchUser(3).then((id) => console.log(id))
-cacheFetchUser(3).then((id) => console.log(id))
+cacheFetchUser(3).then((id) => console.log(id));
+cacheFetchUser(3).then((id) => console.log(id));
+cacheFetchUser(3).then((id) => console.log(id));
 
 // Fetch:  3
 // ​// 3
 // ​// 3
 // ​// 3
 
-
-
 function once(f) {
-    let result;
-    let revoked = false;
-    return (...args) => {
-      if (revoked) return result;
-      const r = f(...args);
-      revoked = true;
-      result = r;
-      return r;
-    };
+  let result;
+  let revoked = false;
+  return (...args) => {
+    if (revoked) return result;
+    const r = f(...args);
+    revoked = true;
+    result = r;
+    return r;
+  };
+}
+
+// 惰性函数
+function once(fn) {
+  function o(...args) {
+    const res = fn(...args);
+    o = () => res;
+    return o();
   }
-  
-  // 惰性函数
-  function once(fn) {
-    function o(...args) {
-      const res = fn(...args);
-      o = () => res;
-      return o();
+  return o; // 函数重写
+}
+
+const memoize = function (func, content) {
+  let cache = Object.create(null);
+  content = content || this;
+  return (...key) => {
+    if (!cache[key]) {
+      cache[key] = func.apply(content, key);
     }
-    return o; // 函数重写
-  }
+    return cache[key];
+  };
+};
+// 在当前函数作用域定义了一个空对象，用于缓存运行结果
+// 运用柯里化返回一个函数，返回的函数由于闭包特性，可以访问到cache
+// 然后判断输入参数是不是在cache的中。如果已经存在，直接返回cache的内容，如果没有存在，使用函数func对输入参数求值，然后把结果存储在cache中
